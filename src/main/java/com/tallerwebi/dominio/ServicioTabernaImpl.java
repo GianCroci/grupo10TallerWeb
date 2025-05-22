418f1c9e705bf006345f1b8f7da7d5aba419bf1f
tomas <tomasattino@gmail.com>
2025-05-21 23:52:53 -0300

    Taberna
    Logica, test y vista taberna. Me faltan retoques visuales

--- src/main/java/com/tallerwebi/dominio/ServicioTabernaImpl.java
+++ src/main/java/com/tallerwebi/dominio/ServicioTabernaImpl.java
@@ -2,11 +2,72 @@ package com.tallerwebi.dominio;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
+
 @Service("servicioTaberna")
 public class ServicioTabernaImpl implements ServicioTaberna{
 
-    @Override
-    public Taberna mostrarTaberna() {
-    return null;
+    public Taberna taberna;
+
+    public ServicioTabernaImpl() {
+        this.taberna = new Taberna();
     }
+
+    @Override
+    public String invitarTrago(PersonajeTaberna personaje) {
+        taberna.invitarTrago(personaje);
+        return "Invitaste un trago a " + personaje.name() + ". Total de cervezas invitadas: " + taberna.getCantidadCervezasInvitadas(personaje);
+    }
+
+    @Override
+    public void hacerGuardia(int horasDeGuardia) {
+        PersonajeTaberna personajeActual = taberna.getPersonajePorHora(LocalTime.now());
+        if (personajeActual == PersonajeTaberna.GUARDIA) {
+            System.out.println("Realizaste una guardia de " + horasDeGuardia + " horas y obtuviste oro.");
+        } else {
+            System.out.println("No hay guardia disponible en este momento.");
+        }
+    }
+
+    //devuelve el personaje correspondiente a la hora actual
+    @Override
+    public String  mostrarTaberna() {
+        PersonajeTaberna vistaSegunPersonaje = taberna.getPersonajePorHora(LocalTime.now());
+        return obtenerVistaSegunPersonaje(vistaSegunPersonaje);
+    }
+
+    //devuelve las cervezas invitadas por el personaje
+    @Override
+    public int getCervezasInvitadas(PersonajeTaberna personaje) {
+        return taberna.getCantidadCervezasInvitadas(personaje);
+    }
+
+    @Override
+    public void resetearInvitacionesDiarias() {
+        taberna = new Taberna();
+    }
+
+    //devuelve la vista correspondiente a la hora actual
+    @Override
+    public String obtenerVistaSegunPersonaje(PersonajeTaberna personajeTaberna) {
+
+        switch (personajeTaberna) {
+            case MERCADER:
+                return "mercader.png";
+            case HERRERO:
+                return "herrero.png";
+            case GUARDIA:
+                return "guardia.png";
+            default:
+                return "No se pudo renderizar la vista correctamente";
+        }
+    }
+
+    @Override
+    public PersonajeTaberna obtenerPersonajeDisponible() {
+        LocalTime horaActual = LocalTime.now();
+        return taberna.getPersonajePorHora(horaActual);
+    }
+
+
 }
--- src/main/java/com/tallerwebi/presentacion/ControladorTaberna.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorTaberna.java
@@ -1,8 +1,17 @@
 package com.tallerwebi.presentacion;
 
+
+import com.tallerwebi.dominio.PersonajeTaberna;
 import com.tallerwebi.dominio.ServicioTaberna;
+import com.tallerwebi.dominio.Taberna;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,17 +19,61 @@ public class ControladorTaberna {
 
     private ServicioTaberna servicioTaberna;
 
+    private Taberna taberna;
+
     public ControladorTaberna(ServicioTaberna servicioTaberna) {
 
+        this.taberna = new Taberna();
         this.servicioTaberna = servicioTaberna;
     }
 
+    //muestra la vista de la taberna
     @GetMapping("/taberna")
     public ModelAndView mostrarTaberna() {
-        ModelAndView modelAndView= new ModelAndView("taberna");
-        modelAndView.addObject("taberna", servicioTaberna.mostrarTaberna());
+        ModelMap modelMap = new ModelMap();
+        PersonajeTaberna personajeDisponible = servicioTaberna.obtenerPersonajeDisponible();
+        String imagenParcial = servicioTaberna.obtenerVistaSegunPersonaje(personajeDisponible);
 
-        return modelAndView;
+        modelMap.put("personajeDisponible", personajeDisponible);
+        modelMap.put("imagenParcial", imagenParcial);
+
+        return new ModelAndView("taberna", modelMap);
 
     }
+
+
+    @PostMapping("/invitarTrago")
+    public ModelAndView invitarTrago(@RequestParam("personaje") String personaje) {
+        ModelMap modelMap = new ModelMap();
+
+        PersonajeTaberna personajeDisponible = servicioTaberna.obtenerPersonajeDisponible();
+
+        try {
+            PersonajeTaberna personajeEnum = PersonajeTaberna.valueOf(personaje);
+
+            if (personajeEnum.equals(personajeDisponible)) {
+                servicioTaberna.invitarTrago(personajeEnum);
+                modelMap.put("mensaje", "Has invitado un trago a " + personajeEnum.name() + ". Total de cervezas invitadas:" + servicioTaberna.getCervezasInvitadas(personajeEnum));
+            } else {
+                modelMap.put("mensaje", "No puedes invitar un trago a " + personajeEnum.name() + " en este momento.");
+            }
+
+        } catch (IllegalArgumentException e) {
+            String mensaje = e.getMessage();
+            if (mensaje != null && mensaje.contains("Ya invitaste")) {
+                modelMap.put("mensaje", mensaje);
+            } else {
+                modelMap.put("mensaje", "Personaje no válido.");
+            }
+        }
+
+        String vistaParcial = servicioTaberna.mostrarTaberna();
+        modelMap.put("vistaParcial", vistaParcial);
+        modelMap.put("personajeDisponible", personajeDisponible != null ? personajeDisponible.name() : null);
+
+        return new ModelAndView("taberna", modelMap);
+    }
+
+
+
 }
